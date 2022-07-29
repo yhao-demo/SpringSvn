fa0314552340ea50483186b1e549288705a281b9
yhao <yh_demo@163.com>
2022-07-29 23:39:30 +0800

    Initial commit

---
+++ src/main/java/com/yhao/springsvn/SpringSvnApplication.java
@@ -0,0 +1,13 @@
+package com.yhao.springsvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhao/springsvn/SpringSvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhao.springsvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
